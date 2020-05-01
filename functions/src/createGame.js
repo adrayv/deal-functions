79b2dc36656b01a1996e7786c95186abc9aa400d
Destin Raymundo <draym001@g.ucla.edu>
2020-04-30 20:41:11 -0700

    create endpoint returns initial player info

--- functions/src/createGame.js
+++ functions/src/createGame.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const init = require('./game/init');
+const { Player } = require('./game/entities');
 
 module.exports = functions.https.onRequest(async (req, res) => {
   try {
@@ -11,10 +12,15 @@ module.exports = functions.https.onRequest(async (req, res) => {
         'attach the name of the initial player as a query parameter i.e. ?player=john'
       );
     }
+    const newPlayer = Player(player);
     const docRef = await db
       .collection('games')
-      .add(init.createInitialGameState(player));
-    res.status(200).send({ gameId: docRef.id });
+      .add(init.createInitialGameState(newPlayer));
+    res.status(200).send({
+      gameId: docRef.id,
+      playerId: newPlayer.id,
+      playerName: player.name,
+    });
   } catch (error) {
     console.error('ERROR CREATING GAME', error);
     res.status(400).send(JSON.stringify(error));
--- functions/src/game/init.js
+++ functions/src/game/init.js
@@ -5,16 +5,15 @@ const { Player } = require('./entities');
 
 module.exports = {
   createInitialGameState(initialPlayer) {
-    const firstPlayer = Player(initialPlayer);
     return {
       turn: 0,
       cardsPlayed: 0,
       deck: shuffle(generateDeck()),
       discard: [],
       players: {
-        [firstPlayer.id]: firstPlayer,
+        [initialPlayer.id]: initialPlayer,
       },
-      order: [firstPlayer.id],
+      order: [initialPlayer.id],
       winner: null,
       status: gameStatuses.pending,
       created_at: new Date(),
